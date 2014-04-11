3caf0711ad8027838a14a90822980a2906b84aa8
Nathan <nathanlandis@gmail.com>
2014-04-10 20:30:49 -0500

    Get Rooms

---
+++ HipperChat.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using HipperChat.Core;
+using HipperChat.Core.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HipperChat.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHipChatRoomService core = new HipChatRoomService("");
+
+            var rooms = core.GetRooms();
+
+            foreach (var room in rooms)
+            {
+                System.Console.WriteLine("{0}: {1} ({2})", room.Id, room.Name, room.Links.Self);
+            }
+
+            System.Console.WriteLine("Done");
+            System.Console.Read();
+        }
+    }
+}
---
+++ HipperChat.Core/Rooms/IHipChatService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HipperChat.Core.Rooms
+{
+    public interface IHipChatRoomService
+    {
+        ICollection<Room> GetRooms();
+    }
+
+    public class HipChatRoomService : IHipChatRoomService
+    {
+        private readonly string _apiKey;
+
+        public HipChatRoomService(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public ICollection<Room> GetRooms()
+        {
+            var client = new WebClient();
+            var json = client.DownloadString("https://api.hipchat.com/v2/room?auth_token=" + _apiKey);
+            var response = JsonConvert.DeserializeObject<GetRoomsResult>(json);
+            return response.Items;
+        }
+
+        private class GetRoomsResult
+        {
+            public ICollection<Room> Items { get; set; }
+            public int StartIndex { get; set; }
+            public int MaxResults { get; set; }
+            public Links Links { get; set; }
+        }
+    }
+}
