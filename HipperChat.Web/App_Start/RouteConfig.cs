eb8b14d7bdd8d954071749b11aaa4626df5b6821
Nathan <nathanlandis@gmail.com>
2014-04-10 22:32:13 -0500

    such web app

---
+++ HipperChat.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HipperChat.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "WriteOnlyPage",
+                url: "{apiKey}",
+                defaults: new { controller = "WriteOnly", action = "Index", apiKey = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
